bec1c19af0d3ddd70389a77ec532dc53b0fb70c6
Mate Kovacs <mate@braid.tech>
2023-11-16 16:24:18 +0900

    checkpoint: hand-crafted Frankenstein monster lives

--- python/main.py
+++ python/main.py
@@ -5,8 +5,19 @@ def main():
     day = adt.DayOfTheWeek.Friday
     print(f"My favorite day is {day}")
 
-    complex = adt.ComplexEnum.Str("hello!")
-    print(f"My favorite complex enum is {complex}")
+    favorites = [
+        adt.ComplexEnum.Int(42),
+        adt.ComplexEnum.Float(3.14),
+        adt.ComplexEnum.Str("hello world"),
+    ]
+    for favorite in favorites:
+        match favorite:
+            case adt.ComplexEnum.Int(i=x):
+                print(f"My favorite int is {x}")
+            case adt.ComplexEnum.Float(f=x):
+                print(f"My favorite float is {x}")
+            case adt.ComplexEnum.Str(s=x):
+                print(f"My favorite string is {x}")
 
 
 if __name__ == "__main__":
